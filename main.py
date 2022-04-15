446f0734cf54f1e952eac1f45bd4492b4aa2dd5f
Rareș Mocanu <raresmihaimocanutor@gmail.com>
2022-04-15 22:48:24 +0300

    first update to new assignment

--- main.py
+++ main.py
@@ -3,15 +3,13 @@ import copy
 import os
 import sys
 import time
-start_time=time.time()
 
-#informatii despre un nod din arborele de parcurgere (nu din graful initial)
 
 class NodParcurgere:
     def __init__(self, info, parinte, cost=0, h=0):
         self.info=info
         self.parinte=parinte #parintele din arborele de parcurgere
-        self.g=cost #consider cost=1 pentru o mutare
+        self.g=cost 
         self.h=h
         self.f=self.g+self.h
 
@@ -25,8 +23,12 @@ class NodParcurgere:
         
     def afisDrum(self, out,afisCost=True, afisLung=False): #returneaza si lungimea drumului
         l=self.obtineDrum()
+        k=1
         for nod in l:
+            print("Nod:"+str(k),file=out)
+            k=k+1
             print(str(nod),file=out)
+
         print("Timp: "+str(time.time()-start_time),file=out)
         if afisCost:
             print("Cost: ", self.g,file=out)
@@ -63,6 +65,7 @@ class NodParcurgere:
             sir+="\n"
         sir+="-"*(2*len(self.info)-1)
         return sir
+
     
         
 
@@ -86,6 +89,27 @@ class Graph: #graful problemei
         for i in range(len(stive)-1):
             if len(stive[i])<len(stive[i+1]):
                 scop=False
+        alpha=0
+        for i in range(len(stive)):
+            for j in range(len(stive[i])):
+                if stive[i][j].isalpha():
+                    alpha+=1
+        #numar literele
+        if alpha!=0:
+            alpha_dedicated_stack=False
+            for i in range(len(stive)):
+                if len(stive[i]) and stive[i][0].isalpha():
+                    alpha_dedicated_stack=True
+                    if len(stive[i])!=alpha:#daca n-am toate literele
+                        scop = False
+                    else:
+                        for j in range(len(stive[i])-1): 
+                            if stive[i][j].isalpha() and stive[i][j+1].isalpha() and ord(stive[i][j])<ord(stive[i][j+1]):
+                                scop=False
+                                break
+                            #daca undeva nu-s in ordine alfabetica
+            if not alpha_dedicated_stack:
+                scop=False
         return scop
 
 
@@ -94,8 +118,6 @@ class Graph: #graful problemei
 
 
 
-
-    #va genera succesorii sub forma de noduri in arborele de parcurgere    
 
 
     def genereazaSuccesori(self, nodCurent, tip_euristica="euristica banala"):
@@ -110,11 +132,14 @@ class Graph: #graful problemei
             for j in range(nr_stive): #j = indicele stivei pe care pun blocul 
                 if idx == j: # nu punem blocul de unde l-am luat
                     continue
-                if len(copie_interm[j]) and int(copie_interm[j][-1]) < int(bloc): #daca varful are valoare mai mica decat blocul de pus
+                if not bloc.isalpha() and len(copie_interm[j]) and copie_interm[j][-1].isalpha():
+                    continue#nu pun numar peste litera 
+                if not bloc.isalpha() and len(copie_interm[j]) and not copie_interm[j][-1].isalpha() and int(copie_interm[j][-1]) < int(bloc):
+                 #daca varful are valoare mai mica decat blocul de pus si nu e litera    
                     continue
-                if j>0:#daca exista o stanga
+                if not bloc.isalpha() and j>0:#daca exista o stanga
                     if len(copie_interm[j-1]) > len(copie_interm[j]):#daca in stanga exista o valoare
-                        if int(copie_interm[j-1][-1])<int(bloc):#daca in stanga e o valoare mai mica
+                        if not copie_interm[j-1][-1].isalpha() and int(copie_interm[j-1][-1])<int(bloc):#daca in stanga e o valoare mai mica
                             continue
                 stive_n=copy.deepcopy(copie_interm)#lista noua de stive
                 stive_n[j].append(bloc) # pun blocul
@@ -129,10 +154,34 @@ class Graph: #graful problemei
     def calculeaza_h(self, infoNod, tip_euristica="euristica banala"):
         # euristica banala
         if tip_euristica=="euristica banala":
-            for i in range(len(infoNod)-1):
-                if len(infoNod[i])<len(infoNod[i+1]):
-                    return 0
-            return 1            
+            scop = 1
+            stive = infoNod
+            for i in range(len(stive)-1):
+                if len(stive[i])<len(stive[i+1]):
+                    scop=0
+            alpha=0
+            for i in range(len(stive)):
+                for j in range(len(stive[i])):
+                    if stive[i][j].isalpha():
+                        alpha+=1
+            #numar literele
+            if alpha!=0:
+                alpha_dedicated_stack=False
+                for i in range(len(stive)):
+                    if len(stive[i]) and stive[i][0].isalpha():
+                        alpha_dedicated_stack=True
+                        if len(stive[i])!=alpha:#daca n-am toate literele
+                            scop = 0
+                        else:
+                            for j in range(len(stive[i])-1): 
+                                if stive[i][j].isalpha() and stive[i][j+1].isalpha() and ord(stive[i][j])<ord(stive[i][j+1]):
+                                    scop=0
+                                    break
+                                #daca undeva nu-s in ordine alfabetica
+                if not alpha_dedicated_stack:
+                    scop=0
+            return scop
+            #n-am structura de nod, ci doar informatiile, deci nu pot sa apelez metoda si trebuie sa copiez codul
         elif tip_euristica=="euristica admisibila 1":
             ##Calculez cate stive nu se incadreaza in conditia finala (nu sunt mai inalte decat stiva de la dreapta)
             h=0
@@ -149,9 +198,10 @@ class Graph: #graful problemei
             return h
         else: #tip_euristica=="euristica neadmisibila"
             h=len(infoNod)
-            #adaug valoarea de la baza stivei inmultita cu costul 
+            #adaug valoarea de la baza stivei inmultita cu costul inmultita cu numarul de stive
             for i in range(len(infoNod)):
-                h+=len(infoNod[i][0])*i
+                if(len(infoNod[i])):
+                    h+=len(infoNod[i][0])*i*len(infoNod)
             return h
 
 
@@ -166,12 +216,9 @@ class Graph: #graful problemei
 @stopit.threading_timeoutable(default="timed out")       
 def breadth_first(gr, nrSolutiiCautate, out):
 
-    #in coada vom avea doar noduri de tip NodParcurgere (nodurile din arborele de parcurgere)
     c=[NodParcurgere(gr.start, None)]
     
     while len(c)>0:
-        #print("Coada actuala: " + str(c))
-        #input()
         nodCurent=c.pop(0)
 
         if gr.testeaza_scop(nodCurent):
@@ -186,7 +233,6 @@ def breadth_first(gr, nrSolutiiCautate, out):
 
 @stopit.threading_timeoutable(default="timed out")       
 def a_star(gr, nrSolutiiCautate, tip_euristica,out):
-    #in coada vom avea doar noduri de tip NodParcurgere (nodurile din arborele de parcurgere)
     c=[NodParcurgere(gr.start, None, 0, gr.calculeaza_h(gr.start))]
     
     while len(c)>0:
@@ -204,7 +250,6 @@ def a_star(gr, nrSolutiiCautate, tip_euristica,out):
             i=0
             gasit_loc=False
             for i in range(len(c)):
-                #diferenta fata de UCS e ca ordonez dupa f
                 if c[i].f>=s.f :
                     gasit_loc=True
                     break
@@ -257,6 +302,89 @@ def depth_first_iterativ(gr, nrSolutiiCautate,file):
 		print("**************\nAdancime maxima: ", i,file=file)
 		nrSolutiiCautate=dfi(NodParcurgere(gr.start, None),i, nrSolutiiCautate,file)
 
+@stopit.threading_timeoutable(default="timed out")
+def a_star_opt(gr,out):
+	l_open=[NodParcurgere(gr.start, None, 0, gr.calculeaza_h(gr.start))]
+	
+
+	l_closed=[]
+	while len(l_open)>0:
+		nodCurent=l_open.pop(0)
+		l_closed.append(nodCurent)
+		if gr.testeaza_scop(nodCurent):
+			print("Solutie: ", end="",file=out)
+			nodCurent.afisDrum(out)
+			print("\n----------------\n",file=out)
+			return
+		lSuccesori=gr.genereazaSuccesori(nodCurent)	
+		for s in lSuccesori:
+			gasitC=False
+			for nodC in l_open:
+				if s.info==nodC.info:
+					gasitC=True
+					if s.f>=nodC.f:
+						lSuccesori.remove(s)
+					else:#s.f<nodC.f
+						l_open.remove(nodC)
+					break
+			if not gasitC:
+				for nodC in l_closed:
+					if s.info==nodC.info:
+						if s.f>=nodC.f:
+							lSuccesori.remove(s)
+						else:#s.f<nodC.f
+							l_closed.remove(nodC)
+						break
+		for s in lSuccesori:
+			i=0
+			gasit_loc=False
+			for i in range(len(l_open)):
+				#diferenta fata de UCS e ca ordonez crescator dupa f
+				#daca f-urile sunt egale ordonez descrescator dupa g
+				if l_open[i].f>s.f or (l_open[i].f==s.f and l_open[i].g<=s.g) :
+					gasit_loc=True
+					break
+			if gasit_loc:
+				l_open.insert(i,s)
+			else:
+				l_open.append(s)
+
+def construieste_drum(gr, nodCurent, limita, nrSolutiiCautate,out):
+	if nodCurent.f>limita:
+		return nrSolutiiCautate, nodCurent.f
+	if gr.testeaza_scop(nodCurent) and nodCurent.f==limita :
+		print("Solutie: ",file=out)
+		nodCurent.afisDrum(out)
+		print("\n----------------\n",file=out)
+		nrSolutiiCautate-=1
+		if nrSolutiiCautate==0:
+			return 0,"gata"
+	lSuccesori=gr.genereazaSuccesori(nodCurent)	
+	minim=float('inf')
+	for s in lSuccesori:
+		nrSolutiiCautate, rez=construieste_drum(gr, s, limita, nrSolutiiCautate,out)
+		if rez=="gata":
+			return 0,"gata"
+		if rez<minim:
+			minim=rez
+	return nrSolutiiCautate, minim
+
+@stopit.threading_timeoutable(default="timed out")
+def ida_star(gr, nrSolutiiCautate,out):
+	
+	nodStart=NodParcurgere(gr.start, None, 0, gr.calculeaza_h(gr.start))
+	limita=nodStart.f
+	while True:
+		nrSolutiiCautate, rez= construieste_drum(gr, nodStart,limita,nrSolutiiCautate,out)
+		if rez=="gata":
+			break
+		if rez==float('inf'):
+			break
+		limita=rez
+
+
+
+
 """
 here be the actual program
 """
@@ -278,14 +406,30 @@ for input in lista_fisiere_input:
     print("BFS:\n",file=fout)
     breadth_first(gr,NSOL,fout,timeout=TTL)
     gr=copy.deepcopy(gr_copy)
-    print("A*:\n",file=fout)
-    a_star(gr,NSOL,"euristica banala",fout,timeout=TTL)
+    print("DF:\n",file=fout)
+    depth_first(gr,NSOL,fout,timeout=TTL/10)
     gr=copy.deepcopy(gr_copy)
     print("DFI:\n",file=fout)
     depth_first_iterativ(gr,NSOL,fout,timeout=TTL)
     gr=copy.deepcopy(gr_copy)
-    print("DF:\n",file=fout)
-    depth_first(gr,NSOL,fout,timeout=TTL)
+    print("A*(euristica banala):\n",file=fout)
+    a_star(gr,NSOL,"euristica banala",fout,timeout=TTL)
+    gr=copy.deepcopy(gr_copy)
+    print("A*(euristica admisibila 1):\n",file=fout)
+    a_star(gr,NSOL,"euristica admisibila 1",fout,timeout=TTL)
+    gr=copy.deepcopy(gr_copy)
+    print("A*(euristica admisibila 2):\n",file=fout)
+    a_star(gr,NSOL,"euristica admisibila 2",fout,timeout=TTL)
+    gr=copy.deepcopy(gr_copy)
+    print("A*(euristica neadmisibila):\n",file=fout)
+    a_star(gr,NSOL,"euristica neadmisibila",fout,timeout=TTL)
+    gr=copy.deepcopy(gr_copy)
+    print("A* optimizat:\n",file=fout)
+    a_star_opt(gr,fout,timeout=TTL)
+    gr=copy.deepcopy(gr_copy)
+    print("IDA*:\n",file=fout)
+    ida_star(gr,NSOL,fout,timeout=TTL)
+
     """
     here the calls are finished
     """
